1ed0753f68f799416ad6804744cae86836bbca39
Olimpia Kwiecień <kwiecien@zoho.com>
2020-01-24 20:03:02 +0100

    Updates

---
+++ precache-manifest.fb80a24f70f53aaf856f3a11fd149786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbc982ed2bb6ef4e3bf8880727e0019",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b69bd2e2a9623f919870",
+    "url": "/monsters-rolodex/static/css/main.5f9ea2b4.chunk.css"
+  },
+  {
+    "revision": "88f7a088e2a5c5ad4db2",
+    "url": "/monsters-rolodex/static/js/2.bcfb89b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.bcfb89b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b69bd2e2a9623f919870",
+    "url": "/monsters-rolodex/static/js/main.2440ef7d.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
